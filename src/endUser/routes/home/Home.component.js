fac64f74f57a209b49ecd76a16e593a76a70865a
unknown <bishoymelekwadie@gmail.com>
2019-05-18 13:14:35 +0200

    working on listing events with firestore

--- src/endUser/routes/home/Home.component.js
+++ src/endUser/routes/home/Home.component.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import Spinner from '../../../sharedComponents/Spinner';
 import clientsApi from '../../../api/clientsApi';
+import Button from 'react-bootstrap/Button'
+import * as firebase from 'firebase';
+var serviceAccount = require('../../../firebase.json');
+var app = firebase.initializeApp(
+  serviceAccount);
+
+var db = app.firestore();
+db.collection('events').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
 
 class Home extends Component {
   constructor() {
@@ -45,14 +61,104 @@ class Home extends Component {
     const selfProps = this.props;
     const selfState = this.state;
     return (
-      <div id="sAdminHome">
+      <div id="listing-cards">
         <Spinner loading={selfState.loading}></Spinner>
         <header>
           <h1>Rasidi Dashboard</h1>
         </header>
         <section>
-          <div className="text-right">
-            <button type="button" onClick={this.openModalHandler} className="btn btn-primary">Create/Update New item</button>
+          <div className="container">
+            <div className="row">
+              {/* <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-light">
+                  <div className="card-body">
+                    <h4 className="card-title">Bologna</h4>
+                    <h6 className="card-subtitle mb-2">Emilia-Romagna Region, Italy</h6>
+                    <p className="card-text">It is the seventh most populous city in Italy, at the heart of a metropolitan area of about one million people. </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-secondary text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">Oslo</h4>
+                    <h6 className="card-subtitle mb-2">Oslofjord, Norway</h6>
+                    <p className="card-text">Oslo is the economic and governmental centre of Norway. The city is also a hub of Norwegian trade, banking and industry.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-dark text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">Berlin</h4>
+                    <h6 className="card-subtitle mb-2">Brandenburg, Germany</h6>
+                    <p className="card-text">Berlin is a world city of culture, politics, media and science. Its economy is based on high-tech firms and the service sector.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-primary text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">Bucharest</h4>
+                    <h6 className="card-subtitle mb-2">Romania</h6>
+                    <p className="card-text">Economically, Bucharest is the most prosperous city in Romania and one of the main industrial centres of Eastern Europe. </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-info">
+                  <div className="card-body">
+                    <h4 className="card-title">Madrid</h4>
+                    <h6 className="card-subtitle mb-2">Spain</h6>
+                    <p className="card-text">The city has almost 3.2 million inhabitants and a metropolitan area population of approximately 6.5 million.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-success">
+                  <div className="card-body">
+                    <h4 className="card-title">London</h4>
+                    <h6 className="card-subtitle mb-2">Greater London, England</h6>
+                    <p className="card-text">London is one of the leading global cities in the arts, commerce, education, entertainment, fashion.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-warning">
+                  <div className="card-body">
+                    <h4 className="card-title">Vienna</h4>
+                    <h6 className="card-subtitle mb-2">Austria</h6>
+                    <p className="card-text">Apart from being regarded as the City of Music because of its musical legacy, it is also said to be "The City of Dreams".</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-danger text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">Amsterdam</h4>
+                    <h6 className="card-subtitle mb-2">Netherlands</h6>
+                    <p className="card-text">Originating as a small fishing village, Amsterdam became one of the most important ports in the world.</p>
+                  </div>
+                </div>
+              </div> */}
+
+              <div className="col-12 col-sm-6 col-lg-4">
+                <div className="card bg-transparent">
+                  <div className="card-body">
+                    <h4 className="card-title">Vladivostok</h4>
+                    <h6 className="card-subtitle mb-2">Primorsky Krai, Russia</h6>
+                    <p className="card-text">The city is the home port of the Russian Pacific Fleet and the largest Russian port on the Pacific Ocean.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Button id="loadMoreButton" onClick={this.handleLoadMoreApps} disabled={false} variant="primary">Load More</Button>
           </div>
         </section>
       </div>
--- src/store-redux/Home/actions.js
+++ src/store-redux/Home/actions.js
@@ -20,37 +20,37 @@ const addError = (err) => dispatch => {
 }
 
 
-const getListOfApps = (isDailyList=true,pageNum = 1) => async  dispatch => {
+const getListOfApps = (isDailyList = true, pageNum = 1) => async  dispatch => {
     try {
-        const resToGetList = await clientsApi.getAllApps(isDailyList, pageNum);
-        const resBodyToGetList = await resToGetList.json();
-        if (resToGetList.status === 200 && resBodyToGetList.data) {
-            if (pageNum !== 1) {
-                const dailyVsWeeklyType = isDailyList ? 'APPLICATIONS/GET_ALL/DAILY/UPDATE_LIST_WITH_MORE_ENTRIES':'APPLICATIONS/GET_ALL/WEEKLY/UPDATE_LIST_WITH_MORE_ENTRIES'
-                dispatch({
-                    type: dailyVsWeeklyType,
-                    dataFetched: resBodyToGetList.data,
-                    currentPageNum: resBodyToGetList.pages.current,
-                    totalPagesNum: resBodyToGetList.pages.total
-                });
-            } else {
-                const dailyVsWeeklyType = isDailyList ? 'APPLICATIONS/GET_ALL/DAILY/UPDATE_LIST_WITH_INITIAL_ENTRIES':'APPLICATIONS/GET_ALL/WEEKLY/UPDATE_LIST_WITH_INITIAL_ENTRIES'
-                dispatch({
-                    type: dailyVsWeeklyType,
-                    dataFetched: resBodyToGetList.data,
-                    currentPageNum: resBodyToGetList.pages.current,
-                    totalPagesNum: resBodyToGetList.pages.total
-                });
-            }
-            dispatch({
-                type: 'LOADING/STOP',
-            });
-        } else {
-            dispatch({
-                type: 'ERRORS/ADD_ONE',
-                error: resBodyToGetList.message,
-            });
-        }
+        // const resToGetList = await clientsApi.getAllApps(isDailyList, pageNum);
+        // const resBodyToGetList = await resToGetList.json();
+        // if (resToGetList.status === 200 && resBodyToGetList.data) {
+        //     if (pageNum !== 1) {
+        //         const dailyVsWeeklyType = isDailyList ? 'APPLICATIONS/GET_ALL/DAILY/UPDATE_LIST_WITH_MORE_ENTRIES':'APPLICATIONS/GET_ALL/WEEKLY/UPDATE_LIST_WITH_MORE_ENTRIES'
+        //         dispatch({
+        //             type: dailyVsWeeklyType,
+        //             dataFetched: resBodyToGetList.data,
+        //             currentPageNum: resBodyToGetList.pages.current,
+        //             totalPagesNum: resBodyToGetList.pages.total
+        //         });
+        //     } else {
+        //         const dailyVsWeeklyType = isDailyList ? 'APPLICATIONS/GET_ALL/DAILY/UPDATE_LIST_WITH_INITIAL_ENTRIES':'APPLICATIONS/GET_ALL/WEEKLY/UPDATE_LIST_WITH_INITIAL_ENTRIES'
+        //         dispatch({
+        //             type: dailyVsWeeklyType,
+        //             dataFetched: resBodyToGetList.data,
+        //             currentPageNum: resBodyToGetList.pages.current,
+        //             totalPagesNum: resBodyToGetList.pages.total
+        //         });
+        //     }
+        dispatch({
+            type: 'LOADING/STOP',
+        });
+        // } else {
+        //     dispatch({
+        //         type: 'ERRORS/ADD_ONE',
+        //         error: resBodyToGetList.message,
+        //     });
+        // }
     } catch (error) {
         dispatch({
             type: 'ERRORS/ADD_ONE',

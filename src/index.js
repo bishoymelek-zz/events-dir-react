9b39c329bb19fda774b686d69bfe68e52c4a602d
Melek <bishoymelekwadie@gmail.com>
2019-07-06 14:42:14 +0200

    adding reset css,stop using redux presist

--- src/index.js
+++ src/index.js
@@ -7,23 +7,22 @@ import { Provider } from 'react-redux';
 // Routing (react-router-dom)
 import { BrowserRouter } from 'react-router-dom';
 import store from './store-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 
 // Reset browser styling
-import './reset.scss';
-import 'bootstrap/dist/css/bootstrap.css';
 
 import './index.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store().store}>
-            <PersistGate loading={null} persistor={store().persistor}>
-                <App />
-            </PersistGate>
-        </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store().store}>
+      {/* <PersistGate loading={null} persistor={store().persistor}> */}
+      <App />
+      {/* </PersistGate> */}
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
